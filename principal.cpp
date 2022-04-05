0a323d5d99f9a62091bd35966f3aa67aedf7dc1a
Roy Campos Leigue <102778889+roy-campos@users.noreply.github.com>
2022-04-04 23:17:57 -0400

    Create principal.cpp

---
+++ principal.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int num;
+
+int main(){
+    do {
+        cout << "Mi nombre es Roy Elias Campos Leigue" << endl;
+        cout << "Naci el 29 de enero de 2003 en Sucre-Boliva " << endl;
+        cout << "Actualmente tengo 19 anos y vivo en Sucre-Bolivia" << endl;
+        cout << "Me gradue del colegio Luz y Verdad el 2020" << endl;
+        cout << "Estoy en la carrera de Ingenieria de sistemas de la USFXCH" << endl;
+        cout << "Vivo con mi mama, mis 3 hermanos y tengo un perro" << endl;
+        cout << "Curse un curso de programacion el 2018, desde entonces me intereso la programacion" << endl;
+        cout << "Me gusta ver Docuseries, series de crimenes y peliculas de ciencia ficcion" << endl;
+        cout << "Soy fan de un equipo de futbol en Inglaterra que se llama Chelsea" << endl;
+        cout << "Estoy soltero" << endl;
+        cout << "En mis tiempos libres escucho musica, podcasts, leo, y juego en mi laptop" << endl;
+        cout << "Mido 1.72 cm" << endl;
+        cout << "Soy heterosexual, mi tipo de sangre es O+" << endl;
+        cout << "Utilizo una laptop para programar y hacer mis tareas de la universidad" << endl;
+        cout << "Vivi en Sucre toda mi vida" << endl;
+        cout << "El 2019 viaje a Chile y fue la primera vez que vi el mar" << endl;
+        cout << "No tengo inclinaciones politicas" << endl;
+        cout << "Se hablar ingles, espanol y algunas frases en frances" << endl;
+        cout << "Me gusta aprender sobre otras culturas, sus idiomas, costumbres" << endl;
+        cout << "En mi etapa de colegio participe en las olimpiadas cientificas de fisica" << endl;
+        cout << "Me interesa la astronomia, me gusta investigar sobre como funcionan las cosas" << endl;
+        cout << "No soy vegetariano, no soy intolerante a la lactosa y no soy intolerante al gluten" << endl;
+        cout << "No hice servicio pre-militar" << endl;
+        cout << "  " << endl;
+        cout << "Presione 1 para volver a imprimir la biografia" << endl;
+        cin>>num;
+    } while (num==1);
+    return 0; 
+}
